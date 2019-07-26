8540a522e054a3273dd7e67296dc71cf85b2da30
Alex Chi <iskyzh@gmail.com>
2019-07-26 09:30:22 +0800

    add documentation for instance

--- src/core/Instance.h
+++ src/core/Instance.h
@@ -54,43 +54,118 @@ public:
 
     bool __debug_offline;
 
+    /**
+     * Constructor
+     * id is node id, rpc is an instance of RPCClient
+     * @param id
+     * @param rpc
+     */
     Instance(const string &id, shared_ptr<RPCClient> rpc);
 
+    /**
+     * Initialize raft node
+     */
     void start();
 
+    /**
+     * Run routine tasks
+     */
     void update();
 
+    /**
+     * Generate election timeout
+     * @return election timeout between 150 and 300
+     */
     static TICK generate_timeout();
 
+    /**
+     * Increase term number and begin election
+     */
     void begin_election();
 
+    /**
+     * Send RPCs to nodes in cluster to synchronize logs
+     */
     void sync_log();
 
+    /**
+     * Transform to follower
+     */
     void as_follower();
 
+    /**
+     * Transform to candidate
+     */
     void as_candidate();
 
+    /**
+     * Transform to leader
+     */
     void as_leader();
 
+    /**
+     * Get corresponding role string
+     * @return string of role
+     */
     string get_role_string();
 
+    /**
+     * Update clusters
+     * @param clusters
+     */
     void set_clusters(const vector<Cluster> &clusters);
 
+    /**
+     * Get cluster size
+     * @return cluster size
+     */
     unsigned cluster_size();
 
     // TODO: remove from parameter as it is no longer used
+    /**
+     * Process RPC message
+     * @param from (deprecated)
+     * @param message
+     */
     void on_rpc(const string &from, shared_ptr<Message> message);
 
+    /**
+     * Get term number in message
+     * @param message
+     * @return term number
+     */
     static Term get_term(shared_ptr<Message> message);
 
+    /**
+     * Get sender in message
+     * @param message
+     * @return sender string
+     */
     static string get_from(shared_ptr<Message> message);
 
+    /**
+     * Append entry to leader
+     * @param entry
+     */
     void append_entry(const string &entry);
 
+    /**
+     * Check whether a message is membership change
+     * @param entry
+     */
     void try_membership_change(const string &entry);
 
+    /**
+     * After old-new cluster config is committed, commit new cluster config
+     * (WIP)
+     */
     void resolve_membership_change();
 
+    /**
+     * Check if a node is in current cluster
+     * @param c node id
+     * @return if in cluster, true, else false
+     */
     bool check_in_cluster(const Cluster &c);
 };
 
